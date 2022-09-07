806835576e8e337227a639210696fb68785765b4
ZIHCO <chizobajames21@gmail.com>
2022-09-07 06:37:52 +0000

    Upto V0.3 handled

---
+++ _exec.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _exec - calls fork and execve
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+int _exec(char **argv)
+{
+	pid_t child_pid = fork();
+	int status;
+
+	if (child_pid == -1)
+	{
+		perror("./hsh");
+		return (1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+			perror("./hsh");
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	return (0);
+}
