4166ecfcd3ad17b0f6ac2111169bd311d7898540
ZIHCO <chizobajames21@gmail.com>
2022-09-07 14:35:36 +0000

    v0.2

--- main.c
+++ main.c
@@ -10,27 +10,26 @@ int main(void)
 	char *buff = NULL;
 	size_t len = 0;
 	FILE *stream = stdin;
-	char *argv[2] = {"./hsh", NULL};
+	int wc;
+	char **argv;
 	ssize_t nread = 0;
 
 	while (nread >= 0)
 	{
 		if (isatty(STDIN_FILENO))
-			_puts("#cisfun$ ");
+			_puts("($) ");
 		nread = getline(&buff, &len, stream);
-		if (buff[_strlen(buff) - 1] == '\n')
-			buff[_strlen(buff) - 1] = '\0';
-		argv[0] = buff;
 		if (nread == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				_puts("\n");
 			break;
 		}
+		wc = wordcount(buff);
+		argv = splitstr(buff, " \n\t", wc);
+		argv[0] = findpath(argv[0]);
 		if (argv[0] != NULL)
-		{
 			_exec(argv);
-		}
 	}
 	free(buff);
 	exit(0);
