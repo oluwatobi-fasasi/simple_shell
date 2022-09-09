846dd727e369d34cfd062ef8b8f9a77ba40ce271
ZIHCO <chizobajames21@gmail.com>
2022-09-09 23:29:26 +0000

    Everything is failing

--- _write.c
+++ _write.c
@@ -16,20 +16,3 @@ int _puts(const char *str)
 	}
 	return (0);
 }
-
-/**
- * _perror - writes error to STDERR
- * @str: a string as argument
- * Return: 0 on success
- */
-int _perror(const char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		write(STDERR_FILENO, &str[i], 1);
-		i++;
-	}
-	return (0);
-}
